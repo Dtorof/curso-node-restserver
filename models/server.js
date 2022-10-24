98c383c221bb27815fb95fb99a509c4bd11f33fb
dtorof <dtoro.sistemas@gmail.com>
2022-10-24 00:44:56 -0500

    Backen basico

---
+++ models/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors');
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        //Con esto se pueden visiaulizar todas las rutas
+        this.usuariosPath = '/api/usuarios';
+
+        //Middlewares
+        this.middelwares();
+        //rutas de la aplicacion
+        this.routes();
+    }
+
+    middelwares(){
+         //CORS 
+         this.app.use(cors())
+        //Directorio publico
+        this.app.use(express.static('public'));
+         //Lectura y Parseo del body
+         this.app.use(express.json());
+
+
+    }
+
+    routes(){
+        this.app.use( this.usuariosPath, require('../routes/user'))
+    }
+
+    listen(){
+        this.app.listen(this.port, ()=>{
+            console.log('servidor correindo en  el puerto', this.port)
+        });
+    }
+}
+
+module.exports = Server;
